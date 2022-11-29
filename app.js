6a0bb7196bf2f168c2459fd055d317e49d0f60c1
arm2349 <56358399+arm2349@users.noreply.github.com>
2022-11-29 00:27:59 -0800

    Update app.js

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
+app.get('/*', function(req,res) {
+		res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 app.use(
   session({
